c855d1d9539788621e56d1e1cf55791ce76483bb
Saintone <79251571+Saintone-lab@users.noreply.github.com>
2026-05-09 17:17:32 +0700

    bwastartup v1.0.1
    Login Update

--- handler/user.go
+++ handler/user.go
@@ -21,7 +21,7 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
 
-		errorMessage := gin.H{"errors": helper.FormatError(err)}
+		errorMessage := gin.H{"errors": helper.FormatValidationError(err)}
 
 		response := helper.APIResponse("Invalid input", 422, "error", errorMessage)
 		c.JSON(422, response)
@@ -42,5 +42,27 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	// }
 	formatter := user.FormatUser(newUser, "token")
 	response := helper.APIResponse("User registered successfully", 200, "success", formatter)
+	c.JSON(200, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Invalid input", 422, "error", errorMessage)
+		c.JSON(422, response)
+		return
+	}
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": []string{err.Error()}}
+		response := helper.APIResponse("Login failed", 422, "error", errorMessage)
+		c.JSON(422, response)
+		return
+	}
+	formatter := user.FormatUser(loggedinUser, "token")
+	response := helper.APIResponse("Login successful", 200, "success", formatter)
 	c.JSON(200, response)
 }
